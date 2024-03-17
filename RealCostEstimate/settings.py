92e31b120c7004c7dd395979201020939cd64646
appricotblue <git2@aindriya.co.in>
2024-03-17 15:03:47 +0530

    second commit

--- RealCostEstimate/settings.py
+++ RealCostEstimate/settings.py
@@ -127,9 +127,9 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-STATICFILES_DIRS=[
-os.path.join(BASE_DIR,'static')
-]
+# STATICFILES_DIRS=[
+# os.path.join(BASE_DIR,'static')
+# ]
 STATIC_URL  = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 
