78753390f5f8f932a719b41b9536d47cab094f81
appricotblue <git2@aindriya.co.in>
2024-03-18 11:28:36 +0530

    installed cors-header

--- RealCostEstimate/settings.py
+++ RealCostEstimate/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'RealCostApp'
+    'RealCostApp',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
@@ -48,6 +49,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
 ]
 
 ROOT_URLCONF = 'RealCostEstimate.urls'
